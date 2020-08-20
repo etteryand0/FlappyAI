b0e355b031675e60fe429800749cb87312996c21
etteryand0 <etteryand@gmail.com>
2020-08-20 20:54:57 +0900

    added jump()

--- bird.py
+++ bird.py
@@ -1,12 +1,10 @@
 class Bird():
   def __init__(self):
-    self.position = [2,100] # [x,y]
+    self.position = [2,2] # [x,y]
 
   def __str__(self):
     return "BirdApp"
 
   def jump(self):
-    pass
-
-  def check_touch(self): # in Game.loop
-    pass # return True/False
+    self.position[1] -= 2
+    return True
